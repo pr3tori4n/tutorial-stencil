1139ae4ad3b4de74ab9960cd2762ee494bc5a928
Harry Robbins <hrobbins@esri.com>
2019-05-28 12:08:03 -0700

    first commit

---
+++ src/components/my-first-component.tsx
@@ -0,0 +1,21 @@
+import { Component, Prop, h, Host } from '@stencil/core';
+
+@Component({
+    tag: 'my-first-component',
+    shadow: true,
+    styleUrl: 'my-first-component.css'
+})
+export class myComponent {
+    @Prop() name: string;
+
+    render() {
+        return (
+            <Host>
+                <slot></slot>
+                <p>
+                    My name is {this.name}
+                </p>
+            </Host>
+        )
+    }
+}
